1fd9c29350ec6df2833c1a7b30839f372620c3b2
lintao <lintao51@gmail.com>
2020-02-26 13:58:26 +0800

    decode the event.

--- demo/OECController.cpp
+++ demo/OECController.cpp
@@ -11,12 +11,20 @@ bool OECController::initialize() {
 
     std::cout << "sizeof(Event)"
               << sizeof(Event) << std::endl;
+
+    // initializing
+
     return true;
 }
 
 const uint32_t* OECController::execute(const uint32_t* buffer) {
 
+    Event event;
+    DataDecoding::decode(buffer, event);
 
+    // more calculation
+
+    // decision
 
     return 0;
 }
